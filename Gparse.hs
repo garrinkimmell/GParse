9fdec20f4689001861dd3a5923872b206e522f80
Garrin Kimmell <garrin.kimmell@gmail.com>
2012-02-20 15:57:26 -0600

    Immediate left-recursion elimination working.

--- Gparse.hs
+++ Gparse.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE NoMonomorphismRestriction, TransformListComp #-}
 module Gparse where
 
 
@@ -12,9 +12,11 @@ import Control.Applicative hiding ((<|>),many)
 import Data.Functor.Identity
 import Data.Maybe(catMaybes)
 import Control.Monad(guard)
-import Data.List(partition,intersperse)
-import Data.Char(isAlpha)
+import Data.List(partition,intersperse,groupBy,sortBy)
+import Data.Char(isAlpha,isAlphaNum)
+import Data.Maybe(fromJust)
 
+import GHC.Exts(the,groupWith)
 -- emptyDef   :: Token.GenLanguageDef st
 emptyDef    = Token.LanguageDef
                { Token.commentStart   = "(%"
@@ -100,7 +102,7 @@ language = do g <- grammar
               let (jmtsParsers,jmts) = unzip rawJs
               let premiseParser = choice (map try jmtsParsers) <?> "Judgment or Formula"
               js' <- mapM (fixRawRules premiseParser) jmts <?> "Fixing Rules"
-              return $ (Language g js',premiseParser)
+              return $ (Language g js', premiseParser)
 
 
 type Grammar = [NonTerm]
@@ -191,25 +193,55 @@ data AST = Leaf String |
            MetaVar String String
          deriving Show
 
+
+
 -- Take a grammar definition, and turn it into a bunch of parsers, one
 -- for each nonterminal.
 makeTermParsers :: Grammar -> M.Map String (Parser AST)
 makeTermParsers g = parsers
   where parsers = M.fromList (concatMap parseNonterm g)
         nonterms = nontermNames g
-        parseNonterm :: NonTerm -> [(String,Parser AST)]
-        parseNonterm (NonTerm ns rhss _) = let p = choice $ (map metavar ns) ++ (map parseRHS rhss)
-                                         in [(n,p) | n <- ns]
 
-        parseRHS :: RHS -> Parser AST
-        parseRHS (RHS tokens cons _) = Node cons <$> sequence (map parseToken tokens)
+
+        -- Build a parser for a particular non-terminal
+        parseNonterm :: NonTerm -> [(String,Parser AST)]
+        parseNonterm (NonTerm ns rhss _) =
+          let isLeftRecursive (RHS (e:es) _ _) = (stripMeta e) `elem` ns
+              isLeftRecursive r = error $ show r
+              (rec,base) = partition isLeftRecursive rhss
+              rec' = [RHS es b r | RHS (e:es) b r <- rec]
+              baseParser = choice $ map metavar ns ++ [parseRHS r | r <- base]
+              restParser = choice [parseRHS' r | r <- rec']
+              p = do hd <- baseParser
+                     rest <- many restParser
+                     return $ foldl (\l r -> r l) hd rest
+
+          in  [(n,p) | n <- ns]
+
+
+        parseRHS ::  RHS -> Parser AST
+        parseRHS (RHS tokens cons _) =
+          Node cons <$> sequence (map parseToken tokens)
+
+        parseRHS' :: RHS -> Parser (AST -> AST)
+        parseRHS' (RHS tokens cons _) = do
+          args <- sequence (map parseToken tokens)
+          return $ \arg -> Node cons (arg:args)
+
         parseToken :: String -> Parser AST
         parseToken t
-          | t `S.member` nonterms = let ~(Just p) = M.lookup t parsers in p
+          | (stripMeta t) `S.member` nonterms =
+            let ~(Just p) = M.lookup (stripMeta t) parsers in p
           | otherwise = Leaf <$> symbol t
+
+        -- Remove the metavariable indices. FIXME: This is temporary,
+        -- until we handle index variables properly.
+        stripMeta t = let t'= takeWhile isAlpha t
+                      in if t' `S.member` nonterms then t' else t
 
 metavar nm = do symbol nm
                 suffix <- many (digit <|> char '\'')
+                whiteSpace
                 return $ MetaVar nm suffix
 
 
@@ -224,7 +256,8 @@ makeJudgmentParser name tokens termParsers  = Node name <$> mapM mkTokenParser t
             ([],_) -> t
             (p,_) -> p
 
-
+-- Given a bunch of judgments with unparsed premises, run the
+-- judgments parser on each premisew.
 fixRawRules :: Parser AST -> Judgment (SourcePos,String) -> Parser (Judgment AST)
 fixRawRules parser (Judgment a b c rules homs) = do
   flip (Judgment a b c) homs  <$> mapM (fixRawRule parser) rules
@@ -243,13 +276,15 @@ parseGrammar fname = do
   f <- readFile fname
   case runIdentity (runPT (whiteSpace *> language <* eof) () fname f) of
     Left err ->  fail (show err)
-    Right (Language grammar judgments,parser) -> return $ (grammar, judgments, parser)
+    Right (Language grammar judgments,parser) ->
+      return $ (grammar, judgments, makeTermParsers grammar)
 
 
 -- Printing
 
 
 run p s = runIndent "<test>" (runPT (whiteSpace *> p <* whiteSpace <* eof) () "<test>" s)
+run' p s = (runPT (whiteSpace *> p <* whiteSpace <* eof) () "<test>" s)
 test  = unlines [
   "grammar",
   "t :: '_foo' ::=  {{com What is this }}",
@@ -259,3 +294,59 @@ test  = unlines [
   "\t| x  :: :: App"
 
   ]
+
+
+
+-- Refactoring junk.
+exprGrammar = [NonTerm ["expr"] [RHS ["expr", "+", "expr"] "Add" [],
+                                RHS ["expr", "-", "expr"] "Sub" [],
+                                RHS ["num"] "Num" []]
+
+               [],
+               NonTerm ["num"] [RHS ["z"] "Zero" [],
+                                RHS ["s","num"] "Suc" []
+                               ] []
+              ]
+
+
+data Factored = FLeaf RHS
+              | FCommon String Factored
+              | FAlt [Factored] deriving Show
+
+
+-- factor :: NonTerm -> [NonTerm]
+norec nt@(NonTerm ns rhs homs) = (removed, simple)
+  where isLeftRecursive (RHS (e:es) _ []) = e `elem` ns
+        (rec,base) = partition isLeftRecursive rhs
+        simple = NonTerm (map (++ "_term") ns) base homs
+        removed = NonTerm ns
+                  [RHS ((n ++ "_term"):es) nm hom | RHS (e:es) nm hom <- rec, n <- ns] []
+
+factorNT (NonTerm ns rhs _) = (ns,factor rhs)
+
+
+
+factor [r] = FLeaf r
+factor rhs = FAlt (map mkNode rest)
+  where groupHead (RHS (x:xs) _ _) (RHS (y:ys) _ _) = x == y
+        groupHead _ _ = False
+        ruleHead (RHS (x:_) _ _) = x
+        ruleTail (RHS (_:xs) a b) = RHS xs a b
+        compareHead (RHS (x:xs) _  _) (RHS (y:ys) _ _) = compare x y
+        mkNode (n,[r]) = FLeaf r
+        mkNode (n,ts) = FCommon n (factor (map ruleTail ts))
+        rest = [ (head (head tokens), rule) | rule@(RHS tokens _ _) <- rhs, then group by (head tokens) using groupWith]
+
+parseRHS :: [AST] -> RHS -> Parser AST
+parseRHS accum (RHS tokens cons _) = do
+  args <- sequence (map parseToken tokens)
+  return (Node cons (accum ++ args))
+
+parseToken t = Leaf <$> symbol t
+
+parseFactored :: [AST] -> Factored -> Parser AST
+parseFactored accum (FLeaf rhs) = parseRHS accum rhs
+parseFactored accum (FCommon t rest) = do
+  tast <- parseToken t
+  parseFactored (accum ++ [tast]) rest
+parseFactored accum (FAlt fs) = choice (map (parseFactored accum) fs)
